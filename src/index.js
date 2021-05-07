9a69539d52be9fe64d5b65f470576a68537e6c3a
hiro <hmaeyama0510@gmail.com>
2021-05-08 08:44:44 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite food list</h1>
+    <ul>
+      <li>Pizza</li>
+      <li>Sukiyaki</li>
+      <li>Ramen</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
