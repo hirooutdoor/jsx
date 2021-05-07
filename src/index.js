5d3111ffc8574a2d74f3a26283132a79cc2b38d5
hiro <hmaeyama0510@gmail.com>
2021-05-08 08:48:00 +0900

    Use JS in HTML in JS(JSX)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Hiro";
+const number = 8;
+
 ReactDOM.render(
   <div>
-    <h1>My favorite food list</h1>
-    <ul>
-      <li>Pizza</li>
-      <li>Sukiyaki</li>
-      <li>Ramen</li>
-    </ul>
+    <h1>Hello {name} !</h1>
+    <p>Your lucky number is {number}.</p>
   </div>,
   document.getElementById("root")
 );
