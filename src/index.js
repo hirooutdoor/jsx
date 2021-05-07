7d6db7a566c3ece0e74fc5f202ab492bab9279ba
hiro <hmaeyama0510@gmail.com>
2021-05-08 08:56:09 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Hiro";
-const number = 8;
+const fname = "Hirofumi";
+const lname = "Maeyama";
+const number = Math.floor(Math.random() * 10 + 1);
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name} !</h1>
+    <h1>
+      Hello, {fname} {lname}!
+    </h1>
     <p>Your lucky number is {number}.</p>
   </div>,
   document.getElementById("root")
